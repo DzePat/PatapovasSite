3a9b5a8e4f87e91d3aef3eeaf06335d9e3674622
Dzedas ingmanLT <ddzedas@gmail.com>
2025-06-09 18:26:00 +0200

    small change for the authentication of the dashboard

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -3,32 +3,34 @@ import { useEffect,useState} from "react";
 
 function Dashboard() {
   const { getAccessTokenSilently, isAuthenticated } = useAuth0();
-  const [message, setMessage] = useState<string>();
-  const audience = import.meta.env.VITE_AUTH0_BACKEND_AUDIENCE;
-
-  useEffect(() => {
-  const fetchData = async () => {
-    try {
-      const token = await getAccessTokenSilently({
-      authorizationParams: { audience }
-    });
-    console.log(token);
-        const res = await fetch("http://localhost:3000/api/private", {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        });
-
-        const data = await res.json();
-        setMessage(data.message);
-      } catch (err) {
-        console.error("Failed to fetch protected data:", err);
-      }
-    };
-    fetchData();
-  }, [getAccessTokenSilently]);
+  if(isAuthenticated){
+    const [message, setMessage] = useState<string>();
+    const audience = import.meta.env.VITE_AUTH0_BACKEND_AUDIENCE;
   
-  console.log("message", message)
+    useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const token = await getAccessTokenSilently({
+        authorizationParams: { audience }
+      });
+      console.log(token);
+          const res = await fetch("http://localhost:3000/api/private", {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          });
+  
+          const data = await res.json();
+          setMessage(data.message);
+        } catch (err) {
+          console.error("Failed to fetch protected data:", err);
+        }
+      };
+      fetchData();
+    }, [getAccessTokenSilently]);
+    
+    console.log("message", message)
+  }
  return (      
     <div className="mainpage">
        <p> This is my dashboard </p>
