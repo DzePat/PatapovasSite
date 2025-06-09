7d3b62e863a989a8b474f41142efe61fbea51b04
Dzedas ingmanLT <ddzedas@gmail.com>
2025-06-09 18:42:06 +0200

    added missing trailing slash

--- src/index.jsx
+++ src/index.jsx
@@ -16,7 +16,7 @@ root.render(
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-          redirect_uri: `${window.location.origin}/dashboard`,
+          redirect_uri: `${window.location.origin}/dashboard/`,
           audience: `${import.meta.env.VITE_AUTH0_AUDIENCE}`
       }}
     >
